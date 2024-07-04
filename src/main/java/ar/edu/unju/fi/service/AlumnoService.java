a8071cb650c598da07dce1fca5ba1b4bab053358
Sebasti348 <valeriosebastian26@gmail.com>
2024-07-04 14:17:50 -0300

    Finalizada Inscripcion.ValerioSebastian

--- src/main/java/ar/edu/unju/fi/service/AlumnoService.java
+++ src/main/java/ar/edu/unju/fi/service/AlumnoService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import ar.edu.unju.fi.dto.AlumnoDTO;
 import ar.edu.unju.fi.model.Alumno;
+import ar.edu.unju.fi.model.Materia;
 @Service
 public interface AlumnoService {
 	public void guardarAlumno(Alumno alumno);
@@ -13,4 +14,6 @@ public interface AlumnoService {
 	public void borrarAlumno(String lu);
 	public void modificarAlumno(Alumno alumnoModificado);
 	public Alumno buscarAlumno(String lu);
+	public void inscribirAlumno(Alumno alumno, Materia materia);
+
 }
