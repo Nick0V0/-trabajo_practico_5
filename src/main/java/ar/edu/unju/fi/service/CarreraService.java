68266729762fc2ac70f57dad61920a6ff3674230
Emilio <RaenMoami>
2024-07-04 05:24:47 -0300

    Validacion de Carrera y Trazas de Carrera.

--- src/main/java/ar/edu/unju/fi/service/CarreraService.java
+++ src/main/java/ar/edu/unju/fi/service/CarreraService.java
@@ -5,13 +5,14 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import ar.edu.unju.fi.dto.CarreraDTO;
+import ar.edu.unju.fi.model.Carrera;
 
 @Service
 public interface CarreraService {
 	
-	public void guardarCarrera(CarreraDTO carreraDTO);
+	public void guardarCarrera(Carrera carrera);
 	public List<CarreraDTO> mostrarCarreras();
 	public void borrarCarrera (String codigo);
-	public void modificarCarrera (CarreraDTO carrera);
-	public CarreraDTO buscarCarrera(String codigo);
+	public void modificarCarrera (Carrera carrera);
+	public Carrera buscarCarrera(String codigo);
 }
