d6bef15ad8cc6a305d005079bbcef28b80691e53
Emilio <RaenMoami>
2024-07-04 12:28:30 -0300

    Commit De Listado de Alumnos por Carrera.

--- src/main/java/ar/edu/unju/fi/controller/AlumnoController.java
+++ src/main/java/ar/edu/unju/fi/controller/AlumnoController.java
@@ -7,13 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import ar.edu.unju.fi.model.Alumno;
+import ar.edu.unju.fi.model.Carrera;
 import ar.edu.unju.fi.model.Materia;
+import ar.edu.unju.fi.repository.CarreraRepository;
 import ar.edu.unju.fi.service.AlumnoService;
 import ar.edu.unju.fi.service.CarreraService;
 import ar.edu.unju.fi.service.MateriaService;
 import jakarta.validation.Valid;
+import org.springframework.ui.Model;
 
 @Controller
 public class AlumnoController {
@@ -27,6 +31,9 @@ public class AlumnoController {
 	MateriaService materiaService;
 	@Autowired
 	CarreraService carreraService;
+	@Autowired
+	private CarreraRepository carreraRepository;
+	
 	@GetMapping("/formularioAlumno")
 	public ModelAndView getFormAlumno() {
 		
@@ -120,5 +127,19 @@ public class AlumnoController {
 		
 		 return modelView;
 	}
-	
+    @GetMapping("/alumnosPorCarrera")
+    public String mostrarAlumnosPorCarrera(Model model) {
+        model.addAttribute("carreras", carreraRepository.findAll());
+        return "alumnosPorCarrera";
+    }
+
+    @GetMapping("/buscarAlumnosPorCarrera")
+    public String buscarAlumnosPorCarrera(@RequestParam("carrera") String codigoCarrera, Model model) {
+        Carrera carrera = carreraRepository.findById(codigoCarrera).orElse(null);
+        if (carrera != null) {
+            model.addAttribute("listadoAlumnos", carrera.getAlumnos());
+        }
+        model.addAttribute("carreras", carreraRepository.findAll());
+        return "alumnosPorCarrera";
+    }
 }
